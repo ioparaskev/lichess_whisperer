433a421488e10b562631731ac75e6a8d5d9722fc
John Paraskevopoulos <jpr@dialectics.space>
2021-09-21 21:37:42 +0300

    Add 960 games support
    Adds whisperer button support for 960 games

--- whisperer.js
+++ whisperer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Lichess Whisper Switch by ipr
 // @namespace    http://tampermonkey.net/
-// @version      0.3.8
+// @version      0.3.9
 // @description  A simple GreaseMonkey script to toggle auto-whisper on/off and at the same time prepending the current move
 // @author       You
 // @match        https://lichess.org/*
@@ -28,9 +28,11 @@ function getFormattedMoveNumber(){
 
 function gameInProgress(){
     accepted_game_conditions_when_not_playing = ["game__tv", "game__tournament"];
+    game_960_in_progress = (document.getElementsByClassName("game__meta")[0].childNodes[1].childNodes[0].nodeValue == "Chess960 start position: ");
     if (
         document.getElementsByClassName("game__meta")[0].childNodes.length == 1 || 
-        accepted_game_conditions_when_not_playing.some(e=>document.getElementsByClassName("game__meta")[0].childNodes[1].className.includes(e))
+        accepted_game_conditions_when_not_playing.some(e=>document.getElementsByClassName("game__meta")[0].childNodes[1].className.includes(e)) ||
+        game_960_in_progress
        )
     {
       return true;
